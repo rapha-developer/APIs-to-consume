bdae7be56aa480229948731ce8316a7b9420c1f1
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-09-06 22:21:39 -0300

    Add data folder with file with all constants used

---
+++ src/data/constants.js
@@ -0,0 +1,29 @@
+
+export const formInputs = [
+    {
+        id: "searchByWord",
+        name: "searchByWord",
+        type: "text",
+        placeholder: "Write word key [palavra chave] to find advice",
+        label: "Search advice...",
+    }
+];
+export const items = [
+    {
+        url: "#",
+        name: "Home",
+    },
+    {
+        url: "#",
+        name: "About",
+    },
+    {
+        url: "#",
+        name: "Contact",
+    },
+];
+export const findAdviceData = {
+    header: "Find advice by 'term' on api",
+    title: "Advice API",
+}
+export const adviceSubtitle = "╰----╮ Latest advices"
