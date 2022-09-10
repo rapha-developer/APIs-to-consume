cd6cd425b5695133fd4b3b9c87161c59f96cb47d
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-09-10 18:37:04 -0300

    Get information from heroData constant and fill the page

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,12 +1,9 @@
 import blob from '../../assets/blob.svg'
 import styles from './style.module.css'
+import { heroData } from '../../data/constants'
 
 function Hero() {
-    const hero = {
-        subtitle: "Cloneables for WEBFLOW",
-        title: "Hero Sections webflow Cloneable template",
-        description: "Want to receive a monthly email in your inbox with awesome free Webflow cloneables, resources and more? Please submit your email below."
-    }
+    
     return (
         <section className={styles.hero}>
             <img src={blob}  
@@ -14,9 +11,13 @@ function Hero() {
                 alt="Blob background" />
             <div className="container">
                 <div className={styles.hero__content}>
-                    <h5 className={`${styles.hero__subtitle} uppercase`}>{hero.subtitle}</h5>
-                    <h1 className={`${styles.hero__title} capitalize`}>{hero.title}</h1>
-                    <p className={styles.hero__description}>{hero.description}</p>
+                    <h5 className={`${styles.hero__subtitle} uppercase`}>{heroData.subtitle}</h5>
+                    <h1 className={`${styles.hero__title}`}>{heroData.title}</h1>
+                    <p className={styles.hero__description}>
+                        {heroData.description}
+                        <br />
+                        <b>{heroData.short_information}</b>
+                        </p>
                 </div>
             </div>
         </section>
