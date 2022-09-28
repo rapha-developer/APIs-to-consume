c73d2463b441e13d9db8c8b1b45acacd7d585b09
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-09-28 17:29:51 -0300

    Add Company Component section and your styles

---
+++ src/pages/Home/components/Company/Company.jsx
@@ -0,0 +1,21 @@
+import styles from './style.module.css'
+
+function Company(props) {
+    const company = {
+        svg: props.companySvg,
+        url: props.url,
+        altText: props.alt_Text
+    }
+
+    return (
+        <a href={company.url} 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="company__link">
+            <img src={company.svg} 
+                alt={company.altText}
+                className={styles.company__logo} />
+        </a>
+    )
+}
+export default Company
