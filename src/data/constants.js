55d851c2cc5c984d77bd77e689de57c27a8066c5
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-09-28 17:26:16 -0300

    Add constants used in many pages of project

---
+++ src/data/constants.js
@@ -0,0 +1,49 @@
+const createMyProjectTitle = {
+    title: "Our Client",
+    description: "Several selected clients, who already believe in our service."
+};
+
+const createListOfCompanies = [
+    {
+        svg: 'companyTenziesSvg',
+        url: '#',
+        altText: 'Tenzies Game Project Logo',
+    }, 
+    {
+        svg: 'companyCodepenSvg',
+        url: '#',
+        altText: 'Codepen Profile Logo'
+    },
+    {
+        svg: 'companyBehanceSvg',
+        url: '#',
+        altText: 'Behance Profile Logo',
+    }
+];
+const createListOfCardsApi = [
+    {
+        title: 'Business Idea Planning',
+        description: 'We present you a proposal and discuss niffty-gritty like',
+        iconType: 'boxSearch'
+    },
+    {
+        title: 'Financial Planning System',
+        description: 'Protocols apart from aen gage models, pricing billing',
+        iconType: 'wallet'
+    },
+    {
+        title: 'Development Website and App',
+        description: 'Communication protocols apart from engagement models',
+        iconType: 'code'
+    },
+    {
+        title: 'Market Analysis Project',
+        description: 'Protocols apart from aen gage models, pricing billing',
+        iconType: 'chart'
+    }
+];
+export {
+    createMyProjectTitle,
+    createListOfCompanies,
+    createListOfCardsApi
+}
