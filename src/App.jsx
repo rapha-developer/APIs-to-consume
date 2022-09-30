916d70b4e85dee0a6d2c329a6e77d090524353c7
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-09-29 21:32:28 -0300

    Fix bug: change import path files; And call all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './components/navbar/Navbar'
+import HomePage from './pages/Home/HomePage'
+import MyProjects from './pages/Home/components/MyProjects/MyProjects'
+import AllApis from './pages/Home/components/allApis/AllApis'
+import SectionVideo from './pages/Home/components/OurVideo/SectionVideo'
+import AboutUs from './pages/Home/components/About/AboutUs'
+import Subscribe from './pages/Home/components/Subscribe/Subscribe'
+import Footer from './components/footer/Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Vite + React</h1>
+      <Navbar />
+      <HomePage />
+      <MyProjects />
+      <AllApis />
+      <SectionVideo />
+      <AboutUs />
+      <Subscribe />
+      <Footer />
     </div>
   )
 }
