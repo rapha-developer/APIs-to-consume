6739526181018bc85d222cc7abf89d7ec6a4478f
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-01 19:12:58 -0300

    Add attribute onClick to component Button

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -2,10 +2,12 @@ import styles from './style.module.css'
 
 function Button(props) {
     const buttonData = {
-        text: props.text 
+        text: props.text,
+        onClick: props?.onClick
     }
     return (
-        <button className={`${styles.btn} ${styles.btn__primary}`}>{buttonData.text}</button>
+        <button className={`${styles.btn} ${styles.btn__primary}`}
+                onClick={buttonData.onClick}>{buttonData.text}</button>
     )
 }
 export default Button
