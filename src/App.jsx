b0870de445cdf01d65b3f4407c80fbe8e58845fd
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-05 10:51:55 -0300

    Use React-Route-dom to map HomePage, AdvicePage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import Navbar from './components/navbar/Navbar'
 import HomePage from './pages/Home/HomePage'
-import MyProjects from './pages/Home/components/MyProjects/MyProjects'
-import AllApis from './pages/Home/components/allApis/AllApis'
-import SectionVideo from './pages/Home/components/OurVideo/SectionVideo'
-import AboutUs from './pages/Home/components/About/AboutUs'
-import Subscribe from './pages/Home/components/Subscribe/Subscribe'
+import AdvicePage from './pages/Advice/AdvicePage'
 import Footer from './components/footer/Footer'
-import RandomAdvice from './pages/Advice/Random/RandomAdvice'
-import FindAdvice from './pages/Advice/Find/FindAdvice'
 
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,14 +13,10 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
-      <MyProjects />
-      <AllApis />
-      <FindAdvice />
-      <RandomAdvice />
-      <SectionVideo />
-      <AboutUs />
-      <Subscribe />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/advice' element={<AdvicePage />} />          
+      </Routes>
       <Footer />
     </div>
   )
