8c331f620b473d7bf829d690ba8fbf508e19405b
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-06 12:14:31 -0300

    Import RickPage Component and createPagesPosition constant; modify path(s) with createItemsToBuildNavbar constant

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import Footer from './components/footer/Footer'
 
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-
+import RickPage from './pages/RickyMorty/RickPage'
+import { createItemsToBuildNavbar, createPagesPosition } from './data/constants'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/advice' element={<AdvicePage />} />          
+        <Route path={createItemsToBuildNavbar[createPagesPosition.first].url} 
+              element={<HomePage />} />
+        <Route path={createItemsToBuildNavbar[createPagesPosition.second].url} 
+              element={<AdvicePage />} />   
+        <Route path={createItemsToBuildNavbar[createPagesPosition.third].url} 
+              element={<RickPage />} />
       </Routes>
       <Footer />
     </div>
