d390936ab26b8b5fe9a5f99038756e766cf69fb2
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-06 12:11:48 -0300

    Create a new Component called RickPage and your styles

---
+++ src/pages/RickyMorty/RickPage.jsx
@@ -0,0 +1,11 @@
+import FindCharacter from './Find/FindCharacter'
+import styles from './style.module.css'
+
+function RickPage() {
+    return (
+        <div className={styles.rickyPage}>
+            <FindCharacter />
+        </div>
+    )
+}
+export default RickPage
