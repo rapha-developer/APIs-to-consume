580cfd8dfd0f221e9acdb11f368b7cdc491e2e4e
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-06 12:10:00 -0300

    Create a new Component {Feature} called FindCharacter and your styles

---
+++ src/pages/RickyMorty/Find/FindCharacter.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import Input from "../../../components/ui/input/Input"
+import NotFoundRickCharacter from "../components/error/NotFoundRickCharacter"
+import RickForm from "../components/form/RickForm"
+import RickCharactersList from "../components/list/RickCharactersList"
+
+import styles from './style.module.css'
+function FindCharacter() {
+    const findCharacterData = {
+        title: 'Find characters by name',
+        subtitle: 'Rick and Morty is an American adult animated science fiction and comedy series.'
+    }
+    const [charactersFromApi, setCharactersFromApi] = useState([])
+    const collectCharacters = (response) => {
+        setCharactersFromApi(response)
+    }
+    const keyWordToNotFoundCharacter  = 'error'
+    const keywordToListCharacters = 'results'
+    const keysFromResponseApi = Object.keys(charactersFromApi)
+
+    const showNotFoundCharacters = keysFromResponseApi.includes(keyWordToNotFoundCharacter)
+    const showAllCharacters = keysFromResponseApi.includes(keywordToListCharacters)
+
+    return (
+        <section className={styles.findCharacter}>
+            <div className={`${styles.findCharacter__grid} container`}>
+                <div className={styles.firstRow}>
+                    <h2 className={styles.__title}>{findCharacterData.title}</h2>
+                    <p className={styles.__subtitle}>{findCharacterData.subtitle}</p>
+                </div>
+                <div className={styles.secondRow}>
+                    <RickForm 
+                        collectCharacters={collectCharacters}
+                        button__text='Find character'
+                    />
+                </div>
+                <div className={styles.thirdRow}>
+                    {showNotFoundCharacters && 
+                        <NotFoundRickCharacter />
+                    }
+                    {showAllCharacters && 
+                        <RickCharactersList
+                            characters={charactersFromApi.results}
+                        />
+                    }
+                </div>
+            </div>
+        </section>
+    )
+}
+export default FindCharacter
