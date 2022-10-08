70dc8fb4c2d25f0323819266a51868a115912cb1
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-08 12:02:40 -0300

    Add SingleCharacter (feature) Component to RickPage component

--- src/pages/RickyMorty/RickPage.jsx
+++ src/pages/RickyMorty/RickPage.jsx
@@ -1,9 +1,11 @@
 import FindCharacter from './Find/FindCharacter'
+import SingleCharacter from './Single/SingleCharacter'
 import styles from './style.module.css'
 
 function RickPage() {
     return (
         <div className={styles.rickyPage}>
+            <SingleCharacter />
             <FindCharacter />
         </div>
     )
