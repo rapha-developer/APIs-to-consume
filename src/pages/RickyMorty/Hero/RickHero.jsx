6fe00e371a7d261c59675b8242a289b12a5050ec
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-12 21:59:48 -0300

    Create new Component called RickHero and your styles

---
+++ src/pages/RickyMorty/Hero/RickHero.jsx
@@ -0,0 +1,45 @@
+import rickAndMortyBgMin from '../../../assets/RickAndMortyBgMin.png'
+import yellowEllipseSvg from '../../../assets/Ellipse yellow.svg'
+import ellipseCircleSvg from '../../../assets/Ellipse circle.svg'
+import styles from './style.module.css'
+
+function RickHero() {
+    const rickHero = {
+        title: 'Rick & Morty API',
+        description: 'Acompanhe malucas viagens no tempo-espaço e por universos paralelos com Rick, um cientista com problemas com a bebida, e seu neto, Morty, um adolescente não tão brilhante quanto o avô.'
+    }
+    return (
+        <section className={styles.heroSection}>
+            <div className="container">
+                <div className={styles.__row}>
+                    <div className="col">
+                    <img src={rickAndMortyBgMin} 
+                        className={styles.image}
+                        alt="Rick and Morty poster of season five"
+                    />
+                    </div>
+                    <div className="col">
+                        <div className={styles.rickHero__info}>
+                            <h1 className={styles.rickHero__title}>
+                                {rickHero.title}
+                            </h1>
+                            <p className={styles.rickHero__description}>
+                                {rickHero.description}
+                            </p>
+                            <span className={styles.rickHero__Svg}>
+                                <img src={yellowEllipseSvg}
+                                className={styles.homeCircleSvg}
+                                alt="Yellow Circle" />
+                                <img src={ellipseCircleSvg}
+                                className={styles.ellipseCircle}
+                                alt="Red circle" />
+                            </span>
+                        </div>
+                </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+export default RickHero
