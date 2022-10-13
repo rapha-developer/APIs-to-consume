4ea9cb9d9814cf6a412d801f8f6a1a04bdff2644
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-12 21:42:44 -0300

    Modify RickPage Component: add background only to features components (SingleCharacter, FindCharacter); Change import RickHeroImage to RickHero

--- src/pages/RickyMorty/RickPage.jsx
+++ src/pages/RickyMorty/RickPage.jsx
@@ -1,12 +1,16 @@
 import FindCharacter from './Find/FindCharacter'
+import RickHero from './Hero/RickHero'
 import SingleCharacter from './Single/SingleCharacter'
 import styles from './style.module.css'
 
 function RickPage() {
     return (
-        <div className={styles.rickyPage}>
-            <SingleCharacter />
-            <FindCharacter />
+        <div className={styles.rickPage}>
+            <RickHero />
+            <div className={styles.rick__bg}>
+                <SingleCharacter />
+                <FindCharacter />
+            </div>
         </div>
     )
 }
