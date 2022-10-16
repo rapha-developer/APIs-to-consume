1b55ae192dca0ff21d645e022f61e7233ace87f4
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-16 12:39:23 -0300

    Move collectPokemon state (main code) to PokemonPage Component; And import AllPokemon Component to show results from api response

--- src/pages/Pokemon/PokemonPage.jsx
+++ src/pages/Pokemon/PokemonPage.jsx
@@ -1,10 +1,21 @@
 
+import { useState } from 'react'
+import AllPokemon from './components/all/AllPokemon'
 import HeroPokemon from './Hero/HeroPokemon'
 import styles from './style.module.css'
 function PokemonPage() {
+    const [collectPokemon, setCollectPokemon] = useState([])
+    const collectResponseFromGenerationUrl = (response) => {
+        setCollectPokemon(response)
+    }
+    const keyWordToShowAllPokemon = 'name'
+    const keysFromCollectPokemon = Object.keys(collectPokemon)
+    const showAllPokemon = (keysFromCollectPokemon.includes(keyWordToShowAllPokemon)) ? true : false
+
     return (
         <div className="pokemonPage">
-            <HeroPokemon />
+            <HeroPokemon collectResponse={collectResponseFromGenerationUrl} />
+            {showAllPokemon && <AllPokemon pokemon={collectPokemon}/>}
         </div>
     )
 }
