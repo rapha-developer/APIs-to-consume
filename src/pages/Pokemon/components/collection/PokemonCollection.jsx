f8b01205a882a427fbf4fad1b7ad6fa6f729c480
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-15 23:04:07 -0300

    Create a new Component called PokemonCollection. Is the area of pokemonItem's. And your styles

---
+++ src/pages/Pokemon/components/collection/PokemonCollection.jsx
@@ -0,0 +1,24 @@
+
+import PokemonItem from '../item/pokemonItem'
+import styles from './style.module.css'
+function PokemonCollection(props) {
+    const pokemon = {
+        collection: props?.collection || [{message:'Not found collection in props'}]
+    }
+    const pokemon__items = pokemon.collection.map((pokemonItem, key) => {
+        return (
+            <PokemonItem 
+                key={pokemonItem.id}
+                name={pokemonItem.name}
+                image={pokemonItem.sprites.other.dream_world.front_default}
+                types={pokemonItem.types}
+            />
+        );
+    });
+    return (
+        <div className={styles.pokemon__collection}>
+            {pokemon__items}
+        </div>
+    )
+}
+export default PokemonCollection
