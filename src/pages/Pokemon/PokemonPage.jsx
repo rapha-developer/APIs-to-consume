79a800a8c6fdfb40dfff442057f5764c67891b5b
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-15 23:13:07 -0300

    Create a new Component called PokemonPage and your (empty) styles

---
+++ src/pages/Pokemon/PokemonPage.jsx
@@ -0,0 +1,11 @@
+
+import HeroPokemon from './Hero/HeroPokemon'
+import styles from './style.module.css'
+function PokemonPage() {
+    return (
+        <div className="pokemonPage">
+            <HeroPokemon />
+        </div>
+    )
+}
+export default PokemonPage
