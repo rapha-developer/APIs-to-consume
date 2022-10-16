a14d2d47acab3d12c13160cb9f35a75eefe2645a
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-15 22:52:09 -0300

    Create new attribute called data-radius, to set border-radius: circle or square.And change styles to follow the attribute

--- src/components/ui/submit/Submit.jsx
+++ src/components/ui/submit/Submit.jsx
@@ -3,10 +3,12 @@ import styles from './style.module.css'
 function Submit(props) {
     const submit = {
         text: props.text,
+        radius: props?.radius || 'rounded',
         type: 'submit'
     }
     return (
-        <button type={props.type} 
+        <button type={submit.type} 
+                data-radius={submit.radius}
                 className={`capitalize ${styles.btn} ${styles.btn__primary}`}>
             {submit.text}
         </button>
