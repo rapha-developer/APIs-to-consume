b700809ea91f312c15af8cbdb0969850aff2f91a
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-16 12:42:56 -0300

    Remove collectPokemon state (main code), allPokemon and others unnecessary imports. Add alt_text to image pocketMonsterBgPng

--- src/pages/Pokemon/Hero/HeroPokemon.jsx
+++ src/pages/Pokemon/Hero/HeroPokemon.jsx
@@ -1,21 +1,11 @@
-import { useState } from 'react'
 import pocketMonsterBgPng from '../../../assets/PocketMonstersBg.png'
-import Submit from '../../../components/ui/submit/Submit'
-import AllPokemon from '../components/all/AllPokemon'
 import GenerationForm from '../components/generation/GenerationForm'
 import styles from './style.module.css'
-function HeroPokemon() {
+function HeroPokemon(props) {
     const PokeAPI = {
         title: 'PokeAPI',
         description: "All the Pokémon data you'll ever need in one place, easily accessible through a modern RESTful API."
     }
-    const [collectPokemon, setCollectPokemon] = useState([])
-    const collectResponseFromGenerationUrl = (response) => {
-        setCollectPokemon(response)
-    }
-    const keyWordToShowAllPokemon = 'name'
-    const keysFromCollectPokemon = Object.keys(collectPokemon)
-    const showAllPokemon = (keysFromCollectPokemon.includes(keyWordToShowAllPokemon)) ? true : false
 
     return (
         <section className={styles.heroPokemon}>
@@ -26,18 +16,17 @@ function HeroPokemon() {
                             <h1 className={styles.heroPokemon__title}>{PokeAPI.title}</h1>
                             <p className={styles.heroPokemon__description}>{PokeAPI.description}</p>
                             <GenerationForm 
-                                collectPokemon={collectResponseFromGenerationUrl}
+                                collectPokemon={props.collectResponse}
                             />
                         </div>
                     </div>
                     <div className="__col">
                         <img src={pocketMonsterBgPng}
                             className=''
-                            alt="" />
+                            alt="bulbasaur, charmander, squirtle playing on a game boy" />
                     </div>
                 </div>
             </div>
-            {showAllPokemon && <AllPokemon pokemon={collectPokemon} /> }
         </section>
     )
 }
