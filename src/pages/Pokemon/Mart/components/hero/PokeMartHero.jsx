40a14ac0f0e91716b7f42cb327918979c92d0658
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-20 16:16:56 -0300

    Created a new Component called PokeMartHero and your styles

---
+++ src/pages/Pokemon/Mart/components/hero/PokeMartHero.jsx
@@ -0,0 +1,29 @@
+
+import RectangleWithCurveSvg from '../../../../../assets/homeRectangleWithCurve.svg'
+import PokeMart from '../../PokeMart'
+import styles from './style.module.css'
+function PokeMartHero(props) {
+    const pokeHero = {
+        title: props?.title || 'Welcome to PokeMart',
+        description: props?.description || 'A Poke Mart is a place where the player can buy items for their Pokémon. It has people to talk to who can give hints on some of the items. For example, here you can buy ',
+        product: props?.product || '...',
+        items: props?.items || []
+    }
+    return (
+        <div className={styles.pokeHero}>
+            <div className={`container 
+            ${styles.pokeHero__content}`}>
+                <img src={RectangleWithCurveSvg} 
+                    alt="rectangular shape with a curve on one sides sides" 
+                    className={styles.bgRectangleWithCurve} />
+                <div className={styles.pokeHero__info}>
+                    <h2 className={styles.pokeHero__title}>{pokeHero.title}</h2>
+                    <p className={styles.pokeHero__description}>{pokeHero
+                    .description}<span>{pokeHero.product}</span></p>
+                </div>
+            </div>
+            <PokeMart items={pokeHero.items}/>
+        </div>
+    )
+}
+export default PokeMartHero
