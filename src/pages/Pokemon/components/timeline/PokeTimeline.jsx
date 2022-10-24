77796c78b611e933c9435562f04e74f59ee95ab0
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-24 18:58:16 -0300

    Created a new called PokeTimeline. To use for show species attributes; And your styles

---
+++ src/pages/Pokemon/components/timeline/PokeTimeline.jsx
@@ -0,0 +1,35 @@
+
+import PokeTimelineItem from './components/timelineItem/PokeTimelineItem';
+import styles from './style.module.css'
+function PokeTimeline(props) {
+    const timeline = {
+        url: props?.url || '',
+        name: props?.name || 'Not found name',
+        attributes: props?.attributes || []
+    }
+    const timeline__items = timeline.attributes.map((attributeItem, key) => {
+		return (
+            <PokeTimelineItem 
+                key={key}
+                label={attributeItem.label}
+                content={attributeItem.content}
+            />
+		);
+	});
+	return (
+		<div className={styles.timelines}>
+			{timeline__items}
+			<span className={styles.vertical__line}></span>
+			<div className={styles.pokeSingle__sticker}>
+				<div className={styles.sticker__wrapperBackground}>
+					<img
+						src={timeline.url}
+						alt={timeline.name}
+						className={styles.pokeSingle__photo}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+export default PokeTimeline;
