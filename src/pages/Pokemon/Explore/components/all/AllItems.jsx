d6d8fe75780984f5a17ad050f59e7229b20140b2
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-26 12:06:01 -0300

    Create a new Component called AllItems; Used to show items in pokeCollection. And your styles

---
+++ src/pages/Pokemon/Explore/components/all/AllItems.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import PokeItemsCollection from "../collection/PokeItemsCollection";
+import styles from "./style.module.css";
+function AllItems(props) {
+	const pokeList = {
+        name: props?.name || 'Not found name in props',
+		items: props?.items || [],
+        size: props?.items?.length || 0
+	};
+    useEffect(() => {
+        createItemsToBuildCollection()
+    }, [pokeList.items])
+    const [items, setItems] = useState([])
+    function createItemsToBuildCollection() {
+        const itemsPromises = []
+        pokeList.items.map((item) => {
+            itemsPromises.push(fetch(item.url).then(res => res.json()));
+        });
+        Promise.all(itemsPromises)
+            .then(allItems => setItems(allItems))
+            .catch(error => alert('itemPromises not working' + error))
+    }
+    const allItemsHero = {
+		title: `${pokeList.size} Poke items found in this ${pokeList.name} category`,
+		description: `Uma lista de ${pokeList.size} Poke items foram encontrados nesta ${pokeList.name} categoria`
+	}
+	return (
+		<div className={styles.allItems}>
+			<div className="container">
+				<div className={styles.allItems__header}>
+					<h2 className={styles.allItems__title}>
+						{allItemsHero.title}
+					</h2>
+					<p className={styles.allItems__description}>
+						{allItemsHero.description}
+					</p>
+				</div>
+                <PokeItemsCollection collection={items} />
+			</div>
+		</div>
+	);
+}
+export default AllItems;
