7bbba185b9d1d563073f822dd1cfc8ad06489e5d
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-26 11:23:58 -0300

    Create new Shadow state and set it by props; And change the component styles to match the feature

--- src/components/ui/notFound/NotFoundCard.jsx
+++ src/components/ui/notFound/NotFoundCard.jsx
@@ -3,10 +3,12 @@ import errorDogSvg from '../../../assets/Error Dog.svg'
 import styles from './style.module.css'
 function NotFoundCard(props) {
     const notFound = {
-        title: props?.title || 'Search not found any result'
+        title: props?.title || 'Search not found any result',
+        shadow: props?.shadow || 'yes'
     }
     return (
-        <div className={styles.notFound}>
+        <div className={styles.notFound}
+            data-shadow={notFound.shadow}>
             <h3 className={styles.notFound__title}>{notFound.title}</h3>
             <img src={errorDogSvg} 
                 alt="Not found 404 - Error message"
