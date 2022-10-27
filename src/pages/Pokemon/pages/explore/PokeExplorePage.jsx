9d06ab6c95912118e068d827f3a83ff6545a3dbc
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-27 12:19:03 -0300

    Add feature PokeMart to Component page PokeExplorePage; Show items of PokeMart only if usePokeMart.show is true

--- src/pages/Pokemon/pages/explore/PokeExplorePage.jsx
+++ src/pages/Pokemon/pages/explore/PokeExplorePage.jsx
@@ -2,8 +2,18 @@ import { useState } from "react"
 import NotFoundCard from "../../../../components/ui/notFound/NotFoundCard"
 import AllItems from "../../Explore/components/all/AllItems"
 import ExploreItems from "../../Explore/ExploreItems"
-
+import PokeMartHero from "../../Mart/components/hero/PokeMartHero"
+import { createItemsToBuildStoreOfPokeMart, createPagesPosition } from '../../../../data/constants'
 function PokeExplorePage(props) {
+    const usePokeMart = {
+        show:props?.showPokeMart || false,
+        product: props?.product || createItemsToBuildStoreOfPokeMart[createPagesPosition.first].product,
+        items: getItemsToBuildPokeMart(props.product || createItemsToBuildStoreOfPokeMart[createPagesPosition.first].product)
+    }
+    function getItemsToBuildPokeMart(product) {
+        const foundStore = createItemsToBuildStoreOfPokeMart.find((storeItem) => storeItem.product === product)
+        return foundStore.items
+    }
     const [collectItems, setCollectItems] = useState([])
     const [showAllItems, setShowAllItems] = useState(false)
     const [showAnyItems, setShowAnyItems] = useState(false)
@@ -20,6 +30,8 @@ function PokeExplorePage(props) {
     return (
         <div className="explorePage">
             <ExploreItems 
+                title={`Select ${props.type} items`}
+                description={`All the Pokémon ${props.type} items you'll ever need in one place, easily accessible through a modern RESTful API.`}
                 type={props.type}
                 collectResponse={collectResponseFromItems}
                 collectErrors={collectErrorsFromResponseItems}
@@ -35,6 +47,14 @@ function PokeExplorePage(props) {
                         shadow='no'
                     />
                 </div>
+            }
+            {usePokeMart.show && <div style={{marginBottom: '100px'}}> 
+                                <PokeMartHero 
+                                    title={`Welcome to PokeMart-${props.product}`}
+                                    product={props.product}
+                                    items={usePokeMart.items}
+                                />
+                            </div>
             }
         </div>
     )
