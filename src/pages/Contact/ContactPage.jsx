0e3c826054baedc15cb8cf700b047f9771dfd5cd
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-28 10:31:18 -0300

    Create a new Page called ContactPage and your styles

---
+++ src/pages/Contact/ContactPage.jsx
@@ -0,0 +1,12 @@
+
+import ContactHero from "./components/hero/ContactHero"
+import ContactInfo from "./components/info/ContactInfo"
+function ContactPage() {
+    return (
+        <div className="contactPage">
+            <ContactHero />
+            <ContactInfo />
+        </div>
+    )
+}
+export default ContactPage
