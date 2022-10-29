ffdd78e82e29608fd3da9dacb3d4295b0bf40c38
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-29 11:16:53 -0300

    Create a new Component called CompaniesCollection; Used to build second part of component (My Projects). And the styles needed

---
+++ src/pages/Home/components/projects/components/collection/CompaniesCollection.jsx
@@ -0,0 +1,24 @@
+
+import Company from '../../../Company/Company'
+import styles from './style.module.css'
+function CompaniesCollection(props) {
+    const companies = {
+        items: props?.items || []
+    }
+    const company__items = companies.items.map((companyItem, key) => {
+        return (
+            <Company 
+                key={key}
+                companySvg={companyItem.svg}
+                altText={companyItem.altText}
+                url={companyItem.url}
+            />
+        )
+    })
+    return (
+        <div className={`MyProjects__col ${styles.companies__collection}`}>
+            {company__items}
+        </div>
+    )
+}
+export default CompaniesCollection
