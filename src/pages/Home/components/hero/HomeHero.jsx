a088f524c085654a410b3890b040b87a999d65d8
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-29 11:02:52 -0300

    Refactor component HomeHero (Hero Section) with calls from child components; And their respective styles

---
+++ src/pages/Home/components/hero/HomeHero.jsx
@@ -0,0 +1,24 @@
+
+import AreaHero from './components/area/AreaHero'
+import BackgroundHero from './components/background/BackgroundHero'
+import styles from './style.module.css'
+function HomeHero(props) {
+    const heroData = {
+        title: props?.title,
+        description: props?.description,
+    }
+    return (
+        <section className={styles.homeHero}>
+            <div className="container">
+                <div className={styles.homeHero__row}>
+                    <AreaHero 
+                        title={heroData.title}
+                        description={heroData.description}
+                    />
+                    <BackgroundHero />
+                </div>
+            </div>
+        </section>
+    )
+}
+export default HomeHero
