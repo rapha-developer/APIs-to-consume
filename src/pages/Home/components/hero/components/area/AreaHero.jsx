7ce5257f7734b45e3ec72ea2ee2385d126937ff8
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-10-31 12:06:36 -0300

    Import and use of constant createAreaHero. Build new feature handleClick to scroll down after click; And add property line-height to css className .hero__description

--- src/pages/Home/components/hero/components/area/AreaHero.jsx
+++ src/pages/Home/components/hero/components/area/AreaHero.jsx
@@ -1,18 +1,30 @@
 
 import homeGridSvg from '../../../../../../assets/homeGrid.svg'
 import Button from '../../../../../../components/ui/button/Button'
+import { createAreaHero } from '../../../../../../data/constants'
 import styles from './style.module.css'
 function AreaHero(props) {
     const heroData = {
-        title: props?.title || 'A Digital Product Agency',
-        description: props?.description || 'Leading digital agency with solid design and development expertise. We build ready made websites, mobile applications, and elaborate online business services.',
-        button__text: 'Contact now'
+        title: props?.title || createAreaHero.title,
+        description: props?.description || createAreaHero.description,
+        button__text: props?.button__text || createAreaHero.button__text
+    }
+    function handleClick() {
+        const fullHeightOfHeroSection = 608
+        window.scroll({
+            top: fullHeightOfHeroSection,
+            left: 0,
+            behavior: 'smooth',
+
+        });
     }
     return (
         <div className={`homeHero__col ${styles.__columnHero}`}>
             <h1 className={styles.hero__title}>{heroData.title}</h1>
             <p className={styles.hero__description}>{heroData.description}</p>
-            <Button text={heroData.button__text} />
+            <Button
+                onClick={handleClick}
+                text={heroData.button__text} />
             <img src={homeGridSvg}
                 className={styles.hero__grid}
                 alt="Many points which compose beautiful grid" />
