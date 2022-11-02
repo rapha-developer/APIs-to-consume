613455504836f5d9e9a49cde4688ba250b80e78a
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-11-02 13:30:17 -0300

    Remove object heroData and directly pass the props to Component AreaHero

--- src/pages/Home/components/hero/HomeHero.jsx
+++ src/pages/Home/components/hero/HomeHero.jsx
@@ -3,18 +3,15 @@ import AreaHero from './components/area/AreaHero'
 import BackgroundHero from './components/background/BackgroundHero'
 import styles from './style.module.css'
 function HomeHero(props) {
-    const heroData = {
-        title: props?.title,
-        description: props?.description,
-    }
     return (
         <section className={styles.homeHero}
                 id='home-hero-id'>
             <div className="container">
                 <div className={styles.homeHero__row}>
                     <AreaHero 
-                        title={heroData.title}
-                        description={heroData.description}
+                        title={props.title}
+                        description={props.description}
+                        button__text={props.button__text}
                     />
                     <BackgroundHero />
                 </div>
