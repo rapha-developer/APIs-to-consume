7104649b0ce52b50b6803911b27c38e36a140c24
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-11-02 13:26:01 -0300

    Create a new Object AdviceHero, with title, description and button__text; Pass all this properties to Component HomeHero

--- src/pages/Advice/AdvicePage.jsx
+++ src/pages/Advice/AdvicePage.jsx
@@ -3,9 +3,18 @@ import FindAdvice from "./Find/FindAdvice"
 import RandomAdvice from "./Random/RandomAdvice"
 
 function AdvicePage() {
+    const adviceHero = {
+        title: 'Advice Slip API',
+        description: 'The Advice Slip JSON API is provided for free. 😎 It currently gives out over 10 million pieces of advice every year. Made by Tom Kiss, purely to satisfy a lifelong disappointment felt when being asked this question by cash machines. ',
+        button__text: 'Check features below'
+    }
     return (
         <div className='advicePage'>
-            <HomeHero />
+            <HomeHero
+                title={adviceHero.title}
+                description={adviceHero.description}
+                button__text={adviceHero.button__text}
+            />
             <RandomAdvice />
             <FindAdvice />
         </div>
